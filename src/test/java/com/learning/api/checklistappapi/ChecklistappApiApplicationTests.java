2aa127a50292f2515eba9e5e9cf56e50f6954819
Willians Augusto <wiliansaugusto@gmail.com>
2021-12-22 10:52:46 -0300

    frist commit

---
+++ src/test/java/com/learning/api/checklistappapi/ChecklistappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.api.checklistappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
